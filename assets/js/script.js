95df6d864406845cf0f548b976e9cd30354d4df4
iamtheasad <iamtheasad@gmail.com>
2019-12-23 20:14:42 +0600

    opinion slider opinion_nav previous-next button added

---
+++ assets/js/script.js
@@ -0,0 +1,148 @@
+// Slider with picture thumbnail
+$(document).ready(function () {
+  var bigimage = $(".header_slider");
+  var thumbs = $("#thumbs");
+  //var totalslides = 10;
+  var syncedSecondary = true;
+
+  bigimage
+    .owlCarousel({
+      items: 1,
+      slideSpeed: 2000,
+      nav: true,
+      autoplay: false,
+      dots: false,
+      loop: true,
+      responsiveRefreshRate: 200,
+      navText: [
+        '<i class="fas fa-arrow-left"></i>',
+        '<i class="fas fa-arrow-right"></i>'
+      ]
+    })
+    .on("changed.owl.carousel", syncPosition);
+
+  thumbs
+    .on("initialized.owl.carousel", function () {
+      thumbs
+        .find(".owl-item")
+        .eq(0)
+        .addClass("current");
+    })
+    .owlCarousel({
+      items: 1,
+      dots: false,
+      nav: true,
+      navText: [
+        '<i class="fas fa-arrow-left"></i>',
+        '<i class="fas fa-arrow-right"></i>'
+      ],
+      smartSpeed: 200,
+      slideSpeed: 500,
+      slideBy: 4,
+      responsiveRefreshRate: 100
+    })
+    .on("changed.owl.carousel", syncPosition2);
+
+  function syncPosition(el) {
+    //if loop is set to false, then you have to uncomment the next line
+    //var current = el.item.index;
+
+    //to disable loop, comment this block
+    var count = el.item.count - 1;
+    var current = Math.round(el.item.index - el.item.count / 2 - 0.5);
+
+    if (current < 0) {
+      current = count;
+    }
+    if (current > count) {
+      current = 0;
+    }
+    //to this
+    thumbs
+      .find(".owl-item")
+      .removeClass("current")
+      .eq(current)
+      .addClass("current");
+    var onscreen = thumbs.find(".owl-item.active").length - 1;
+    var start = thumbs
+      .find(".owl-item.active")
+      .first()
+      .index();
+    var end = thumbs
+      .find(".owl-item.active")
+      .last()
+      .index();
+
+    if (current > end) {
+      thumbs.data("owl.carousel").to(current, 100, true);
+    }
+    if (current < start) {
+      thumbs.data("owl.carousel").to(current - onscreen, 100, true);
+    }
+  }
+
+  function syncPosition2(el) {
+    if (syncedSecondary) {
+      var number = el.item.index;
+      bigimage.data("owl.carousel").to(number, 100, true);
+    }
+  }
+
+  thumbs.on("click", ".owl-item", function (e) {
+    e.preventDefault();
+    var number = $(this).index();
+    bigimage.data("owl.carousel").to(number, 300, true);
+  });
+
+});
+
+
+/* Portfolio Isotope */
+
+jQuery(document).ready(function () {
+  // For Isotope card or image auto height 
+  var $portfolioIsotope = $('.portfolio_list').isotope({
+    itemSelector: '.portfolio_item',
+    percentPosition: true,
+    masonry: {
+      columnWidth: '.portfolio_item'
+    }
+  })
+
+  // Connected items between button and isotope item
+  $('.portfolio_isotope_menu').on('click', 'button', function () {
+    var filterValue = $(this).attr('data-filter');
+    $portfolioIsotope.isotope({
+      filter: filterValue
+    });
+  });
+
+  // Button active class added for isotope
+  $('.portfolio_isotope_menu').each(function (i, buttonGroup) {
+    var $buttonGroup = $(buttonGroup);
+    $buttonGroup.on('click', 'button', function () {
+      $buttonGroup.find('.active_portfolio').removeClass('active_portfolio');
+      $(this).addClass('active_portfolio');
+    });
+  });
+});
+
+
+/*Opinion Slick Slider*/
+jQuery(document).ready(function(){
+  $('.opinion_slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    asNavFor: '.opinion_nav',
+    autoplay: true,
+
+  });
+  $('.opinion_nav').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    asNavFor: '.opinion_slider',
+    centerMode: true,
+    focusOnSelect: true,
+  });
+});
